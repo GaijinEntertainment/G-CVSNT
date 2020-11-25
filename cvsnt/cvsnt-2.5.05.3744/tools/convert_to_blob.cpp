3da25306741d6815b69e9c34516e43260bca9f1e
Aliot <anton@gaijin.ru>
2020-11-25 06:11:44 +0300

    cvt tool is now concurrent tool

--- cvsnt/cvsnt-2.5.05.3744/tools/convert_to_blob.cpp
+++ cvsnt/cvsnt-2.5.05.3744/tools/convert_to_blob.cpp
@@ -1,4 +1,4 @@
-//to build on linux: clang++ -std=c++17 -O2 convert_to_blob.cpp blob_operations.cpp ./sha256/sha256.c -lz -lzstd -ocvtblob
+//to build on linux: clang++ -std=c++17 -O2 -I../src repack-blobs.cpp ../src/blob_operations.cpp ../src/sha256/sha256.c -lz -lzstd -orepack-blobs
 
 #include "simpleLock.cpp.inc"
 #include "sha_blob_reference.h"
@@ -8,7 +8,10 @@
 #include <fstream>
 #include <vector>
 #include <ctime>
+#include <atomic>
 #include "zlib.h"
+#include "flags.h"
+#include "concurrent_queue.h"
 #ifdef _WIN32
 namespace fs = std::experimental::filesystem;
 #else
@@ -16,18 +19,6 @@ namespace fs = std::filesystem;
 #endif
 
 
-static void usage()
-{
-  printf("CVS_TMP env var (or /tmp) should point to same filesystem as root\n");
-  printf("Usage: <lock_server> <user_name> <full_root> full_directory file\n");
-  printf("or: <lock_server> <user_name> <full_root> [full_directory]\n");
-  printf("example:convert_to_blob 127.0.0.1 some_user /cvs/some_repo testDir a.png\n");
-  printf("or:convert_to_blob 127.0.0.1 some_user /cvs/some_repo \n");
-  printf("or:convert_to_blob 127.0.0.1 some_user /cvs/some_repo SomeFolderThere\n");
-  printf("Warning! no ,v in the end is needed. Warning, do not enter CVS directories\n");
-  printf("Warning! run only on server!\n");
-  printf("Warning! it can lock one file for a short while, but will do that numerous time. \n");
-}
 static bool fastest_conversion = false;//if true, we won't repack, just calc sha256 and put zlib block as is.
 static void ensure_blob_mtime(const char* verfile, const char *blob_file)
 {
@@ -48,9 +39,191 @@ static void rename_z_to_normal(const char *f)
   }
 }
 
-static uint64_t deduplicatedData = 0, readData = 0, writtenData = 0;
+static std::atomic<uint64_t> deduplicatedData = 0, readData = 0, writtenData = 0;
+static std::mutex lock_id_mutex;
 
-static void process_file(int lock_server_socket, const char *rootDir, const char *dir, const char *file)
+struct ThreadData
+{
+  std::vector<char> fileUnpackedData, filePackedData, sha_file_name;
+};
+
+static int lock_server_socket = -1;
+
+static void process_file_ver(const char *rootDir,
+  std::string &filePath,//v file
+  const std::filesystem::path &path,//blob reference file
+  ThreadData& data, bool mutex_needed)
+{
+  std::vector<char> &fileUnpackedData = data.fileUnpackedData, &filePackedData  = data.filePackedData, &sha_file_name = data.sha_file_name;
+  sha_file_name.resize(filePath.length() + sha256_encoded_size + 7);
+  if (is_blob_reference(rootDir, path.string().c_str(), sha_file_name.data(), sha_file_name.size()))
+  {
+    printf("%s is already blob reference\n", path.string().c_str());
+    //rename_z_to_normal(entry.path().c_str());
+    return;
+  }
+  size_t lockId = 0;
+  auto get_lock = [&](const char *fp)
+  {
+    if (lockId)
+      return;
+    printf("obtaining lock for <%s>...", fp);
+    lockId = 0;
+    do {
+      {
+        std::unique_lock<std::mutex> lockGuard(lock_id_mutex);
+        lockId = do_lock_file(lock_server_socket, filePath.c_str(), 1, 0);
+      }
+      if (lockId == 0)
+        sleep(500);
+    } while(lockId == 0);
+    printf(" lock=%lld, start processing\n", (long long int) lockId);
+  };
+  auto unlock = [&](){
+    if (!lockId)
+      return;
+    printf("releasing lock %lld for %s\n", (long long int) lockId, filePath.c_str());
+    std::unique_lock<std::mutex> lockGuard(lock_id_mutex);
+    do_unlock_file(lock_server_socket, lockId);
+    lockId = 0;
+  };
+
+  get_lock(path.string().c_str());//Lock1
+  if (is_blob_reference(rootDir, path.string().c_str(), sha_file_name.data(), sha_file_name.size()))
+  {
+    //became reference!
+    unlock();//unLock1
+    return;
+  }
+
+  const auto sz = fs::file_size(path);
+  fileUnpackedData.resize(sz);
+  std::ifstream f(path, std::ios::in | std::ios::binary);
+  f.read(fileUnpackedData.data(), sz);
+  unlock();//unLock1, because some other utility can lock
+
+  readData += sz;
+  std::string srcPathString = path.c_str();
+  const bool wasPacked = (srcPathString[srcPathString.length()-1] == 'z' && srcPathString[srcPathString.length()-2] == '#');
+  size_t unpackedDataSize = wasPacked ? size_t(ntohl(*(int*)fileUnpackedData.data())) : sz;
+  //process packed data
+  if (wasPacked)
+  {
+    //printf("was packed %d ->%d\n", (int)unpackedDataSize,(int)sz);
+    //we need to unpack data first, so we can repack.
+    //ofc, for fastest possible conversion we can just write blobs as is (i.e. keep them zlib)
+    std::swap(fileUnpackedData, filePackedData);
+    fileUnpackedData.resize(unpackedDataSize);//we then swap it with fileUnpackedData
+    z_stream stream = {0};
+    inflateInit(&stream);
+    stream.avail_in = filePackedData.size() - sizeof(int);
+    stream.next_in = (Bytef*)(filePackedData.data() + sizeof(int));
+    stream.avail_out = unpackedDataSize;
+    stream.next_out = (Bytef*)fileUnpackedData.data();
+    if (unpackedDataSize && inflate(&stream, Z_FINISH)!=Z_STREAM_END)
+    {
+      fprintf(stderr, "[E] can't unpack %s of sz=%d unpacked=%d \n", srcPathString.c_str(), (int)sz, (int)unpackedDataSize);
+      return;
+    }
+  }
+  //if wasPacked, originally packed data is in filePackedData
+  unsigned char sha256[32];
+  size_t wr = 0;
+  if (fastest_conversion)
+  {
+    calc_sha256(srcPathString.c_str(), fileUnpackedData.data(), fileUnpackedData.size(), false, unpackedDataSize, sha256);
+    const size_t sha_file_name_len = 1024;
+    char sha_file_name[sha_file_name_len];//can be dynamically allocated, as 64+3+1 + strlen(root). Yet just don't put it that far!
+    char sha256_encoded[sha256_encoded_size+1];
+    encode_sha256(sha256, sha256_encoded, sizeof(sha256_encoded));
+    get_blob_filename_from_encoded_sha256(rootDir, sha256_encoded, sha_file_name, sha_file_name_len);
+    if (!isreadable(sha_file_name))// otherwise blob exist
+    {
+      char *temp_filename = NULL;
+      FILE *dest = cvs_temp_file(&temp_filename, "wb");
+      if (!dest)
+        error(1, errno, "can't open write temp_filename <%s> for <%s>(<%s>)", temp_filename, sha_file_name, srcPathString.c_str());
+      BlobHeader hdr = wasPacked ? get_header(zlib_magic, fileUnpackedData.size(), filePackedData.size()-sizeof(int), 0) : get_header(noarc_magic, fileUnpackedData.size(), fileUnpackedData.size(), 0);
+      if (fwrite(&hdr,1,sizeof(hdr),dest) != sizeof(hdr))
+        error(1, 0, "can't write temp_filename <%s> for <%s>(<%s>) of %d len", temp_filename, sha_file_name, srcPathString.c_str(), (uint32_t)sizeof(hdr));
+      if (fwrite(wasPacked ? filePackedData.data() + sizeof(int) : fileUnpackedData.data(), 1, hdr.compressedLen, dest) != hdr.compressedLen)
+        error(1, 0, "can't write temp_filename <%s> for <%s>(<%s>) of %d len", temp_filename, sha_file_name, srcPathString.c_str(), (uint32_t)hdr.compressedLen);
+      create_dirs(rootDir, sha256);
+      change_file_mode(temp_filename, 0666);
+      rename_file (temp_filename, sha_file_name, false);//we dont care if blob is written independently
+      fclose(dest);
+      blob_free (temp_filename);
+      wr = hdr.compressedLen + sizeof(hdr);
+    } else
+    {
+      ensure_blob_mtime(srcPathString.c_str(), sha_file_name);//so later incremental repack still works correctly
+    }
+  } else
+    wr = write_binary_blob(rootDir, sha256, path.c_str(), fileUnpackedData.data(), fileUnpackedData.size(), BlobPackType::FAST, false);
+
+  //now we can write reference
+  if (!wr)
+  {
+    printf("deduplication %d for %s", int(sz), filePath.c_str());
+    deduplicatedData += sz;
+  }
+
+  get_lock(path.string().c_str());//lock2
+  if (write_blob_reference(path.c_str(), sha256, false))
+  {
+    if (wasPacked)
+      rename_z_to_normal(path.c_str());
+  } else
+    fprintf(stderr, "[E] blob reference %s couldn't be saved\n", path.c_str());
+  unlock();//unlock2
+  writtenData += wr;
+}
+
+struct ProcessTask
+{
+  std::string filePath;
+  std::filesystem::path path;
+};
+
+struct FileVerProcessor
+{
+  FileVerProcessor():queue(&threads){}
+  void finishDownloads()
+  {
+    queue.finishWork();
+  }
+
+  bool is_inited() const {return !threads.empty();}
+  void init(const char *root, int threads_count)
+  {
+    base_repo = root;
+    for (int ti = 0; ti < threads_count; ++ti)
+      threads.emplace_back(std::thread(processor_thread_loop, this));
+  }
+  void wait();
+
+  static void processor_thread_loop(FileVerProcessor *processor)
+  {
+    ProcessTask task;
+    ThreadData data;
+    while (processor->queue.wait_and_pop(task))
+      process_file_ver(processor->base_repo.c_str(), task.filePath, task.path, data, true);
+  }
+  void emplace(const std::string &filePath, const std::filesystem::path &p)
+  {
+    if (!is_inited())
+      return;
+    queue.emplace(ProcessTask{filePath, p});
+  }
+  std::vector<std::thread> threads;//soa
+
+  concurrent_queue<ProcessTask> queue;
+  std::string base_repo;
+};
+
+static FileVerProcessor processor;
+
+static void process_file(const char *rootDir, const char *dir, const char *file)
 {
   printf("process <%s>/<%s>\n", dir, file);
   std::string dirPath = rootDir;
@@ -68,132 +241,20 @@ static void process_file(int lock_server_socket, const char *rootDir, const char
   }
 
   std::string filePath = dirPath + file;
-  std::vector<char> fileUnpackedData, filePackedData;
-  std::vector<char> sha_file_name(filePath.length() + sha256_encoded_size + 7);
+  ThreadData data;
   for (const auto & entry : fs::directory_iterator(pathToVersions))
   {
     if (entry.is_directory())
       continue;
-    if (is_blob_reference(rootDir, entry.path().string().c_str(), sha_file_name.data(), sha_file_name.size()))
-    {
-      printf("%s is already blob reference\n", entry.path().string().c_str());
-      //rename_z_to_normal(entry.path().c_str());
-      continue;
-    }
-
-    size_t lockId = 0;
-    auto get_lock = [&](const char *fp)
-    {
-      if (lockId)
-        return;
-      printf("obtaining lock for <%s>...", fp);
-      lockId = 0;
-      do {
-        lockId = do_lock_file(lock_server_socket, filePath.c_str(), 1, 0);
-        if (lockId == 0)
-          sleep(500);
-      } while(lockId == 0);
-      printf(" lock=%lld, start processing\n", (long long int) lockId);
-    };
-    auto unlock = [&](){
-      if (!lockId)
-        return;
-      printf("releasing lock %lld for %s\n", (long long int) lockId, filePath.c_str());
-      do_unlock_file(lock_server_socket, lockId);
-      lockId = 0;
-    };
-
-    get_lock(entry.path().string().c_str());
-    if (is_blob_reference(rootDir, entry.path().string().c_str(), sha_file_name.data(), sha_file_name.size()))
-    {
-      //became reference
-      unlock();
-      continue;
-    }
     //read data
-    const auto sz = fs::file_size(entry.path());
-    fileUnpackedData.resize(sz);
-    std::ifstream f(entry.path(), std::ios::in | std::ios::binary);
-    f.read(fileUnpackedData.data(), sz);
-    unlock();
-    readData += sz;
-    std::string srcPathString = entry.path().c_str();
-    const bool wasPacked = (srcPathString[srcPathString.length()-1] == 'z' && srcPathString[srcPathString.length()-2] == '#');
-    size_t unpackedDataSize = wasPacked ? size_t(ntohl(*(int*)fileUnpackedData.data())) : sz;
-    //process packed data
-    if (wasPacked)
-    {
-      //printf("was packed %d ->%d\n", (int)unpackedDataSize,(int)sz);
-      //we need to unpack data first, so we can repack.
-      //ofc, for fastest possible conversion we can just write blobs as is (i.e. keep them zlib)
-      std::swap(fileUnpackedData, filePackedData);
-      fileUnpackedData.resize(unpackedDataSize);//we then swap it with fileUnpackedData
-      z_stream stream = {0};
-      inflateInit(&stream);
-      stream.avail_in = filePackedData.size() - sizeof(int);
-      stream.next_in = (Bytef*)(filePackedData.data() + sizeof(int));
-      stream.avail_out = unpackedDataSize;
-      stream.next_out = (Bytef*)fileUnpackedData.data();
-      if (unpackedDataSize && inflate(&stream, Z_FINISH)!=Z_STREAM_END)
-      {
-        fprintf(stderr, "[E] can't unpack %s of sz=%d unpacked=%d \n", srcPathString.c_str(), (int)sz, (int)unpackedDataSize);
-        continue;
-      }
-    }
-    //if wasPacked, originally packed data is in filePackedData
-    unsigned char sha256[32];
-    size_t wr = 0;
-    if (fastest_conversion)
-    {
-      calc_sha256(srcPathString.c_str(), fileUnpackedData.data(), fileUnpackedData.size(), false, unpackedDataSize, sha256);
-      const size_t sha_file_name_len = 1024;
-      char sha_file_name[sha_file_name_len];//can be dynamically allocated, as 64+3+1 + strlen(root). Yet just don't put it that far!
-      char sha256_encoded[sha256_encoded_size+1];
-      encode_sha256(sha256, sha256_encoded, sizeof(sha256_encoded));
-      get_blob_filename_from_encoded_sha256(rootDir, sha256_encoded, sha_file_name, sha_file_name_len);
-      if (!isreadable(sha_file_name))// otherwise blob exist
-      {
-        char *temp_filename = NULL;
-        FILE *dest = cvs_temp_file(&temp_filename, "wb");
-        if (!dest)
-          error(1, errno, "can't open write temp_filename <%s> for <%s>(<%s>)", temp_filename, sha_file_name, srcPathString.c_str());
-        BlobHeader hdr = wasPacked ? get_header(zlib_magic, fileUnpackedData.size(), filePackedData.size()-sizeof(int), 0) : get_header(noarc_magic, fileUnpackedData.size(), fileUnpackedData.size(), 0);
-        if (fwrite(&hdr,1,sizeof(hdr),dest) != sizeof(hdr))
-          error(1, 0, "can't write temp_filename <%s> for <%s>(<%s>) of %d len", temp_filename, sha_file_name, srcPathString.c_str(), (uint32_t)sizeof(hdr));
-        if (fwrite(wasPacked ? filePackedData.data() + sizeof(int) : fileUnpackedData.data(), 1, hdr.compressedLen, dest) != hdr.compressedLen)
-          error(1, 0, "can't write temp_filename <%s> for <%s>(<%s>) of %d len", temp_filename, sha_file_name, srcPathString.c_str(), (uint32_t)hdr.compressedLen);
-        create_dirs(rootDir, sha256);
-        change_file_mode(temp_filename, 0666);
-        rename_file (temp_filename, sha_file_name, false);//we dont care if blob is written independently
-        fclose(dest);
-        blob_free (temp_filename);
-        wr = hdr.compressedLen + sizeof(hdr);
-      } else
-      {
-        ensure_blob_mtime(srcPathString.c_str(), sha_file_name);//so later incremental repack still works correctly
-      }
-    } else
-      wr = write_binary_blob(rootDir, sha256, entry.path().c_str(), fileUnpackedData.data(), fileUnpackedData.size(), BlobPackType::FAST, false);
-
-    //now we can write reference
-    if (!wr)
-    {
-      printf("deduplication %d for %s", int(sz), filePath.c_str());
-      deduplicatedData += sz;
-    }
-    get_lock(entry.path().string().c_str());
-    if (write_blob_reference(entry.path().c_str(), sha256, false))
-    {
-      if (wasPacked)
-        rename_z_to_normal(entry.path().c_str());
-    } else
-      fprintf(stderr, "[E] blob reference %s couldn't be saved\n", entry.path().c_str());
-    unlock();
-    writtenData += wr;
+    if (processor.is_inited())
+      processor.emplace(filePath, entry.path());
+    else
+      process_file_ver(rootDir, filePath, entry.path(), data, false);
   }
 }
 
-static void process_directory(int lock_server_socket, const char *rootDir, const char *dir)
+static void process_directory(const char *rootDir, const char *dir)
 {
   printf("process dir <%s>\n", dir);
   std::string dirPath = rootDir;
@@ -208,7 +269,7 @@ static void process_directory(int lock_server_socket, const char *rootDir, const
       if (strcmp(entry.path().filename().c_str(), "CVS") != 0 &&
           strcmp(entry.path().filename().c_str(), "CVSROOT") != 0 &&
           strcmp(entry.path().filename().c_str(), BLOBS_SUBDIR_BASE) != 0)
-        process_directory(lock_server_socket, rootDir, entry.path().lexically_relative(rootDir).c_str());
+        process_directory(rootDir, entry.path().lexically_relative(rootDir).c_str());
     } else
     {
       std::string filename = entry.path().filename();
@@ -216,7 +277,7 @@ static void process_directory(int lock_server_socket, const char *rootDir, const
       if (filename.length()>2 && filename[filename.length()-1] == 'v' && filename[filename.length()-2] == ',')
       {
         filename[filename.length()-2] = 0;
-        process_file(lock_server_socket, rootDir, dir, filename.c_str());
+        process_file(rootDir, dir, filename.c_str());
       }
     }
   }
@@ -226,26 +287,40 @@ static void process_directory(int lock_server_socket, const char *rootDir, const
 
 int main(int ac, const char* argv[])
 {
-  if (ac < 4)
-  {
-    printf("not enough params <%d>\n", ac);
-    usage();
-    exit(1);
+  auto options = flags::flags{ac, argv};
+  options.info("convert_to_blob",
+    "conversion utility from old CVS to blob de-duplicated one"
+  );
+
+  auto lock_url = options.arg("-lock_url", "Url for lock server");
+  auto lock_user = options.arg("-user", "User name for lock server");
+  auto rootDir = options.arg("-root", "Root dir for CVS");
+  auto tmpDir = options.arg_or("-tmp", "", "Tmp dir for blobs");
+  auto dir = options.arg_or("-dir", "", "Folder to process (inside root)");
+  auto file = options.arg_or("-file", "", "File to process (inside dir)");
+  auto threads = options.arg_as_or<int>("-j", 0,"concurrency level(threads to run)");
+
+  bool help = options.passed("-h", "print help usage");
+  if (help || !options.sane()) {
+    std::cout << options.usage();
+    return help ? 0 : -1;
   }
   tcp_init();
-  const char *rootDir = argv[3];
-  int lock_server_socket = lock_register_client(argv[2], rootDir, argv[1]);
+  lock_server_socket = lock_register_client(lock_url.c_str(), rootDir.c_str(), lock_user.c_str());
   if (lock_server_socket == -1)
   {
     fprintf(stderr, "[E] Can't connect to lock server\n");
     exit(1);
   }
-  if (ac > 5)
+  if (threads>1)
+    processor.init(rootDir.c_str(), threads);
+
+  if (file.length()>0)
   {
-    process_file(lock_server_socket, rootDir, argv[4], argv[5]);
+    process_file(rootDir.c_str(), dir.c_str(), file.c_str());
   } else
   {
-    process_directory(lock_server_socket, rootDir, ac > 4 ? argv[4] : "");
+    process_directory(rootDir.c_str(), dir.c_str());
   }
   printf("written %g mb, saved %g mb, de-duplicated %g mb\n", double(writtenData)/(1<<20),
     (double(readData)-double(writtenData))/(1<<20),
---
+++ cvsnt/cvsnt-2.5.05.3744/tools/flags.h
@@ -0,0 +1,208 @@
+#include <algorithm>
+#include <sstream>
+#include <string>
+#include <tuple>
+#include <utility>
+#include <vector>
+
+namespace flags {
+
+using namespace std::string_literals;
+
+namespace detail {
+
+std::vector<std::string> split(std::string s, std::string delim) {
+    std::vector<std::string> result;
+    size_t pos = 0;
+    std::string token;
+    while ((pos = s.find(delim)) != std::string::npos) {
+        token = s.substr(0, pos);
+        result.push_back(token);
+        s.erase(0, pos + delim.length());
+    }
+    result.push_back(s);
+    return result;
+}
+
+enum class flag_type {
+    optional,
+    required,
+    flag,
+};
+
+} // namespace detail
+
+class flags {
+  public:
+    flags(int argc, const char** argv) : argc_(argc), argv_(argv) {}
+
+    void info(std::string program_name, std::string description) {
+        program_name_ = program_name;
+        description_ = description;
+    }
+
+    bool passed(std::string flag, std::string det) {
+        flags_.emplace_back(flag, detail::flag_type::flag, ""s, det);
+        return passed_(flag);
+    }
+
+    std::string arg(std::string flag, std::string det) {
+        flags_.emplace_back(flag, detail::flag_type::required, ""s, det);
+        return arg_(flag);
+    }
+
+    std::vector<std::string> args(std::string flag, std::string det) {
+        flags_.emplace_back(flag, detail::flag_type::optional, "[]", det);
+
+        auto pos = std::find(argv_, argv_ + argc_, flag);
+        std::vector<std::string> result;
+        if (pos == argv_ + argc_ || pos + 1 == argv_ + argc_) {
+            return result;
+        }
+        pos++;
+        while (pos != argv_ + argc_ && *pos[0] != '-') {
+            result.push_back(std::string(*pos));
+            pos++;
+        }
+
+        return result;
+    }
+
+    template <typename T>
+    T arg_as(std::string flag, std::string det) {
+        flags_.emplace_back(flag, detail::flag_type::required, "", det);
+
+        auto value = std::stringstream(arg_(flag));
+        T x = {};
+        value >> x;
+        return x;
+    }
+
+    template <typename T>
+    std::vector<T> args_as(std::string flag, std::string det) {
+        flags_.emplace_back(flag, detail::flag_type::optional, "[]", det);
+
+        auto parts = detail::split(arg_(flag), ",");
+        std::vector<T> xs;
+        for (auto part : parts) {
+            auto value = std::stringstream(part);
+            T x = {};
+            value >> x;
+            xs.push_back(x);
+        }
+
+        return xs;
+    }
+
+    template <typename T>
+    T arg_as_or(std::string flag, T alt, std::string det) {
+        flags_.emplace_back(flag, detail::flag_type::optional,
+                            std::to_string(alt), det);
+
+        if (!passed_(flag)) {
+            return alt;
+        }
+        auto value = std::stringstream(arg_(flag));
+        T x = {};
+        value >> x;
+        return x;
+    }
+
+    std::string arg_or(std::string flag, std::string alt, std::string det) {
+        flags_.emplace_back(flag, detail::flag_type::optional, alt, det);
+
+        if (!passed_(flag)) {
+            return alt;
+        }
+        return arg_(flag);
+    }
+
+    bool required_arguments(const std::vector<std::string>& args) {
+        for (auto& arg : args) {
+            if (!passed_(arg)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool sane() {
+        for (auto [flag, type, alt, detail] : flags_) {
+            (void)alt;
+            if (type == detail::flag_type::required && !passed_(flag)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    std::string usage() {
+        auto flag_name = [](const std::string &flag, const detail::flag_type type, const std::string &alt) {
+            auto output = std::stringstream("");
+            output << flag;
+            if (type == detail::flag_type::optional)
+                output << "[=" << alt << "]" ;
+             else if (type == detail::flag_type::flag)
+                output << "flag";
+            else
+                output << "    ";
+            return output.str();
+        };
+        auto flag_name2 = [&](auto& f) {
+          return flag_name(std::get<0>(f), std::get<1>(f), std::get<2>(f));
+        };
+        auto max_flag_size = flag_name2(*std::max_element(
+            flags_.begin(), flags_.end(), [&](auto& lhs, auto& rhs) {
+                return flag_name2(lhs).size() < flag_name2(rhs).size();
+            })).size();
+        auto output = std::stringstream("");
+
+        if (program_name_.empty()) {
+            program_name_ = argv_[0];
+        }
+        output << program_name_ << "\n";
+        if (!description_.empty()) {
+            output << description_ << "\n";
+        }
+        output << "\n";
+
+        output << "USAGE: " << program_name_ << " [OPTIONS]\n\n";
+
+        output << "OPTIONS:\n";
+        for (auto [flag, type, alt, detail] : flags_) {
+            output << "  ";
+            auto flag_print = flag_name(flag, type, alt);
+            output << flag_print;
+            auto padding = max_flag_size - flag_print.size();
+            output << std::string(padding, ' ');
+            if (detail.length()>0)
+                output << "| " << detail;
+
+            output << "\n";
+        }
+        return output.str();
+    }
+
+  private:
+    bool passed_(std::string flag) {
+        return std::find(argv_, argv_ + argc_, flag) != (argv_ + argc_);
+    }
+
+    std::string arg_(std::string flag) {
+        auto pos = std::find(argv_, argv_ + argc_, flag);
+        if (pos == argv_ + argc_ || pos + 1 == argv_ + argc_) {
+            return "";
+        }
+        pos++;
+        return std::string(*pos);
+    }
+
+    int argc_;
+    const char** argv_;
+    typedef std::tuple<std::string, detail::flag_type, std::string, std::string> flag_t;
+    std::vector<flag_t> flags_;
+    std::string program_name_;
+    std::string description_;
+};
+
+} // namespace flags
