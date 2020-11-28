03cf1a5fb7713bc224a966ea7c320fc221d3838e
Aliot <anton@gaijin.ru>
2020-11-28 05:17:27 +0300

    fix typo in blake3 h

--- cvsnt/cvsnt-2.5.05.3744/src/blake3/blake3_impl.h
+++ cvsnt/cvsnt-2.5.05.3744/src/blake3/blake3_impl.h
@@ -53,7 +53,7 @@ enum blake3_flags {
   #define BLAKE3_REQUIRE_AVX512
 #elif __AVX2__ && !defined(BLAKE3_NO_AVX2)
   #define BLAKE3_REQUIRE_AVX2
-#elif  !defined(BLAKE3_NO_SSE41);
+#elif _SSE4_1__ && !defined(BLAKE3_NO_SSE41)
   #define BLAKE3_REQUIRE_SSE41
 #else
   #define BLAKE3_REQUIRE_SSE2
