8a6615886aae56b86dc505fc6a584ca769610a55
root <root@lv-ro-cvsvm-07.gaijin.lan>
2020-12-27 16:57:04 +0300

    report based on files

--- cvsnt/cvsnt-2.5.05.3744/tools/convert_to_blob.cpp
+++ cvsnt/cvsnt-2.5.05.3744/tools/convert_to_blob.cpp
@@ -561,7 +561,7 @@ void process_db(const char *rootDir, const db_map &db)
   }
   rcs_files_map.clear();
   printf("converting RCS\n");
-  std::atomic<int> processed = 0;
+  std::atomic<int> processed = 0, last_processed = 0;
   const size_t cnt = rcs_files.size();
   #pragma omp parallel for
   for (size_t i = 0; i < cnt; i++)
@@ -585,9 +585,13 @@ void process_db(const char *rootDir, const db_map &db)
     #endif
     prepare_strings(rootDir, dir.c_str(), file.c_str(), dirPath, pathToVersions, filePath, rcsFilePath);
     actual_rcs_replace(file.c_str(), rcsFilePath.c_str(), rcsFilePath, pathToVersions, rcs_map.second, false);
-    const int cProcessed = processed++;
-    if ((cProcessed&1023) == 1023)
-      printf("processed %d/%d\n",cProcessed, cnt);
+    processed += rcs_map.second.size();
+    const int cProcessed = processed.load();
+    if (cProcessed > last_processed.load()+4096)
+    {
+      last_processed = cProcessed;
+      printf("processed %d/%d\n",cProcessed, entries);
+    }
   }
 }
 
