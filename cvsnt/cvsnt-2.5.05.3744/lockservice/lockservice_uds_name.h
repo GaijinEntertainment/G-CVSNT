32542a5d0dc2d05269290b3a680b0b9dc6432f29
Aliot <anton@gaijin.ru>
2021-02-24 18:07:27 +0300

    change UDS path for lockserver to /tmp back again

--- cvsnt/cvsnt-2.5.05.3744/lockservice/lockservice_uds_name.h
+++ cvsnt/cvsnt-2.5.05.3744/lockservice/lockservice_uds_name.h
@@ -1,2 +1,2 @@
 #pragma once
-static const char * uds_lock_socket_name = "/var/run/cvs/lockserver_addr.sock";
+static const char * uds_lock_socket_name = "/tmp/lockserver_addr.sock";//you can change path, but filename is used for auto-detection of UDS
