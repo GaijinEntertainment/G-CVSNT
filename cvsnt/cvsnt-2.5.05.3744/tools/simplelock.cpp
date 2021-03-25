b44f7704eac09752749a6e1b81f05442d433b9aa
Aliot <anton@gaijin.ru>
2021-03-25 22:41:28 +0300

    lockservice monitoring tool

--- cvsnt/cvsnt-2.5.05.3744/tools/simplelock.cpp
+++ cvsnt/cvsnt-2.5.05.3744/tools/simplelock.cpp
@@ -2,36 +2,37 @@
 static void usage()
 {
   printf("Usage: <server> <user> <full_root/> (<full_directory/file,v> | <lock_id))\n");
-  printf("Or: <server> <user> <full_root/> monitor\n");
+  printf("Or: <server> (for monitoring)\n");
   printf("example:simplelock 127.0.0.1 some_user /home/some_user/test/ testDir/a.png,v\n");
 }
 
 int main(int ac, const char* argv[])
 {
-  if (ac < 4)
+  if (ac < 2)
   {
-    printf("%d args 4 needed\n", ac);
+    printf("%d args 2 needed\n", ac);
     usage();
     exit(1);
   }
   tcp_init();
-  int lock_server_socket = lock_register_client(argv[2], argv[3], argv[1]);
-  if (lock_server_socket == -1)
-  {
-    printf("Can't connect\n");
-    exit(1);
-  }
+  int lock_server_socket = -1;
   if (ac < 5)
   {
-    printf("monitoring");
-    print_status(lock_server_socket);
+    printf("monitoring\n");
+    print_status(argv[1]);
   } else
   {
+    lock_server_socket = lock_register_client(argv[2], argv[3], argv[1]);
+    if (lock_server_socket == -1)
+    {
+      printf("Can't connect\n");
+      exit(1);
+    }
     char fullPath[512];
     snprintf(fullPath, sizeof(fullPath), "%s%s", argv[3], argv[4]);
     size_t lockId = do_lock_file(lock_server_socket, fullPath, 1, 1);
     printf("obtained lock %lld, sleep for 1sec\n", (uint64_t) lockId);
-    sleep_ms(1000);
+    sleep_ms(10000);
     do_unlock_file(lock_server_socket, lockId);
   }
 
