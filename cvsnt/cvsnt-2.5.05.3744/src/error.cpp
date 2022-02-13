a55a1cb2eb6de3feeadf2b0bd7445fc50a5c53f4
Anton Yudintsev <anton@gaijin.team>
2022-02-13 19:19:36 +0200

    CVS: fix: do not exit with acquired mutex

--- cvsnt/cvsnt-2.5.05.3744/src/error.cpp
+++ cvsnt/cvsnt-2.5.05.3744/src/error.cpp
@@ -74,127 +74,135 @@ void error_exit ()
 /* VARARGS */
 #include <mutex>
 static std::recursive_mutex error_mutex;
-void error (int status, int errnum, const char *message, ...)
+void error(int status, int errnum, const char* message, ...)
 {
-    std::unique_lock<std::recursive_mutex> lock(error_mutex);
-	static int in_error = 0;
-
-    int save_errno = errno;
-
-	if(in_error)
+	bool should_exit = false;
 	{
-		if(status)
-			error_exit();
-		return;
-	}
-	in_error = 1;
+		std::unique_lock<std::recursive_mutex> lock(error_mutex);
+		static int in_error = 0;
 
-    if (message[0] != '\0')
-    {
-	va_list args;
-	const char *p;
-	char *q;
-	char *str;
-	int num;
-	long lnum;
-	unsigned int unum;
-	unsigned long ulnum;
-	int ch;
-	char buf[100];
+		int save_errno = errno;
 
-	cvs_outerr (program_name, 0);
-	if (command_name && *command_name)
-	{
-	    cvs_outerr (" ", 1);
-	    if (status != 0)
-		cvs_outerr ("[", 1);
-	    cvs_outerr (command_name, 0);
-	    if (status != 0)
-		cvs_outerr (" aborted]", 0);
-	}
-	cvs_outerr (": ", 2);
+		if (in_error)
+		{
+			if (status)
+				error_exit();
+			return;
+		}
+		in_error = 1;
 
-	VA_START (args, message);
-	p = message;
-	while ((q = (char*)strchr (p, '%')) != NULL)
-	{
-	    static const char msg[] =
-		"\ninternal error: bad % in error()\n";
-	    if (q - p > 0)
-		cvs_outerr (p, q - p);
+		if (message[0] != '\0')
+		{
+			va_list args;
+			const char* p;
+			char* q;
+			char* str;
+			int num;
+			long lnum;
+			unsigned int unum;
+			unsigned long ulnum;
+			int ch;
+			char buf[100];
 
-	    switch (q[1])
-	    {
-	    case 's':
-		str = va_arg (args, char *);
-		if(str)
-			cvs_outerr (str, strlen (str));
+			cvs_outerr(program_name, 0);
+			if (command_name && *command_name)
+			{
+				cvs_outerr(" ", 1);
+				if (status != 0)
+					cvs_outerr("[", 1);
+				cvs_outerr(command_name, 0);
+				if (status != 0)
+					cvs_outerr(" aborted]", 0);
+			}
+			cvs_outerr(": ", 2);
+
+			VA_START(args, message);
+			p = message;
+			while ((q = (char*)strchr(p, '%')) != NULL)
+			{
+				static const char msg[] =
+					"\ninternal error: bad % in error()\n";
+				if (q - p > 0)
+					cvs_outerr(p, q - p);
+
+				switch (q[1])
+				{
+				case 's':
+					str = va_arg(args, char*);
+					if (str)
+						cvs_outerr(str, strlen(str));
+					else
+						cvs_outerr("(null)", 6);
+					break;
+				case 'd':
+					num = va_arg(args, int);
+					sprintf(buf, "%d", num);
+					cvs_outerr(buf, strlen(buf));
+					break;
+				case 'l':
+					if (q[2] == 'd')
+					{
+						lnum = va_arg(args, long);
+						sprintf(buf, "%ld", lnum);
+					}
+					else if (q[2] == 'u')
+					{
+						ulnum = va_arg(args, unsigned long);
+						sprintf(buf, "%lu", ulnum);
+					}
+					else goto bad;
+					cvs_outerr(buf, strlen(buf));
+					q++;
+					break;
+				case 'x':
+					unum = va_arg(args, unsigned int);
+					sprintf(buf, "%x", unum);
+					cvs_outerr(buf, strlen(buf));
+					break;
+				case 'c':
+					ch = va_arg(args, int);
+					buf[0] = ch;
+					cvs_outerr(buf, 1);
+					break;
+				case '%':
+					cvs_outerr("%", 1);
+					break;
+				default:
+				bad:
+					cvs_outerr(msg, sizeof(msg) - 1);
+					/* Don't just keep going, because q + 1 might point to the
+					   terminating '\0'.  */
+					goto out;
+				}
+				p = q + 2;
+			}
+			cvs_outerr(p, strlen(p));
+		out:
+			va_end(args);
+
+			if (errnum != 0)
+			{
+				cvs_outerr(": ", 2);
+				cvs_outerr(strerror(errnum), 0);
+			}
+			cvs_outerr("\n", 1);
+		}
+
+		if (status)
+		{
+			cvs_flushout();
+			cvs_flusherr();
+			should_exit = true;
+		}
 		else
-			cvs_outerr ("(null)", 6);
-		break;
-	    case 'd':
-		num = va_arg (args, int);
-		sprintf (buf, "%d", num);
-		cvs_outerr (buf, strlen (buf));
-		break;
-	    case 'l':
-		if (q[2] == 'd')
 		{
-		    lnum = va_arg (args, long);
-		    sprintf (buf, "%ld", lnum);
+			cvs_flusherr();
+			errno = save_errno;
+			in_error = 0;
 		}
-		else if (q[2] == 'u')
-		{
-		    ulnum = va_arg (args, unsigned long);
-		    sprintf (buf, "%lu", ulnum);
-		}
-		else goto bad;
-		cvs_outerr (buf, strlen (buf));
-		q++;
-		break;
-	    case 'x':
-		unum = va_arg (args, unsigned int);
-		sprintf (buf, "%x", unum);
-		cvs_outerr (buf, strlen (buf));
-		break;
-	    case 'c':
-		ch = va_arg (args, int);
-		buf[0] = ch;
-		cvs_outerr (buf, 1);
-		break;
-	    case '%':
-		cvs_outerr ("%", 1);
-		break;
-	    default:
-	    bad:
-		cvs_outerr (msg, sizeof (msg) - 1);
-		/* Don't just keep going, because q + 1 might point to the
-		   terminating '\0'.  */
-		goto out;
-	    }
-	    p = q + 2;
 	}
-	cvs_outerr (p, strlen (p));
-    out:
-	va_end (args);
-
-	if (errnum != 0)
-	{
-	    cvs_outerr (": ", 2);
-	    cvs_outerr (strerror (errnum), 0);
-	}
-	cvs_outerr ("\n", 1);
-    }
-
-    if (status)
-	{
-		cvs_flushout();
-		cvs_flusherr();
-		error_exit ();
-	}
-	cvs_flusherr();
-    errno = save_errno;
-	in_error = 0;
+	if (should_exit)
+		error_exit();
 }
 
 /* Print the program name and error message MESSAGE, which is a printf-style
